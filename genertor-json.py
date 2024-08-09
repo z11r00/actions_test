eb728c27b83f0748e8f98575f5bc16a3bfcde5d0
fuchao <ZFC19950314>
2024-08-09 20:08:24 +0800

    fix: 600

--- genertor-json.py
+++ genertor-json.py
@@ -1,4 +1,5 @@
 import json  
+import os  
   
 # 创建一个包含一些数据的字典  
 data = {  
@@ -13,8 +14,16 @@ data = {
     }  
 }  
   
+# 定义文件路径  
+file_path = 'rocdatas/202408/data.json'  
+  
+# 确保目录存在  
+directory = os.path.dirname(file_path)  
+if not os.path.exists(directory):  
+    os.makedirs(directory)  
+  
 # 将字典写入到JSON文件中  
-with open('rocdatas/202408/data.json', 'w', encoding='utf-8') as f:  
+with open(file_path, 'w', encoding='utf-8') as f:  
     json.dump(data, f, ensure_ascii=False, indent=4)  
   
 print('JSON文件已生成。')
